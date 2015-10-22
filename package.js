bf2ab66e18aa0bce8e272aecb41340e2c07be8d3
Cristo <cristo.rabani@gmail.com>
2015-10-22 20:17:08 +0200

    Update package.js

--- package.js
+++ package.js
@@ -9,7 +9,7 @@ Package.describe({
 
 Package.onUse(function (api) {
     api.versionsFrom('1.2.0.2');
-    api.use(['universe:modules@0.5.0', 'react-runtime@0.13.3_7', 'ecmascript@0.1.5']);
+    api.use(['universe:modules@0.6.0', 'react-runtime@0.13.3_7', 'ecmascript@0.1.5']);
     api.addFiles([
         'modules/ReactWithAddons.js',
         //mixins
